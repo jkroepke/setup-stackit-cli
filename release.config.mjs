bfd6f000227cb8bb0327476841bc138818ac0525
Jan-Otto Kröpke <mail@jkroepke.de>
2025-05-08 00:55:28 +0200

    Add automated releases
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

---
+++ release.config.mjs
@@ -0,0 +1,25 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "releaseRules": [
+          { "type": "chore", "scope": "deps", "release": "patch" },
+        ]
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        "successComment": false,
+        "labels": false,
+        "addReleases": false,
+      }
+    ]
+  ]
+};
